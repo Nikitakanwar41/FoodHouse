54a4d8714a5555327ca2030542ca4118ecc4086e
Nikitakanwar41 <nikitakanwar441@gmail.com>
2024-10-24 18:57:23 +0530

    change some text

--- FoodHouse/src/Components/Footer/Footer.jsx
+++ FoodHouse/src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className="footer-content">
          <div className="footer-content-left">
           <img className='logo' src={assets.logo} alt="" />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quos illo nobis eveniet accusamus odit quo porro unde aliquid ex!</p>
+          <p>FoodHouse is the one of the best food company that deliver good food all over world and the fastest delivery. it is a very large company famous for it's good quality food.</p>
           <div className="footer-social-icons">
             <img src={assets.facebook_icon}   alt="" />
             <img src={assets.twitter_icon}   alt="" />
@@ -16,16 +16,16 @@ const Footer = () => {
           </div>
          </div>
          <div className="footer-content-center">
-        <h2>Company</h2>
+        <h2>FoodHouse</h2>
         <ul>
-            <li>Home</li>
+            <a href='/'><li>Home</li></a>
             <li>About Us</li>
             <li>Delivery</li>
             <li>Privacy Policy</li>
         </ul>
          </div>
          <div className="footer-content-right">
-        <h2>Get in touch</h2>
+        <h2>Contact here</h2>
         <ul>
             <li>+91 98665884395</li>
             <li>foodhouse41@gmail.com</li>
